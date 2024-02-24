435b4ae6b95693b2435663b54bd3d1278097c151
Abraham <abraham@clarusway.com>
2024-02-24 18:19:32 +0300

    new def

---
+++ t.py
@@ -0,0 +1,26 @@
+def selam() :
+  print("merhaba")
+
+selam()  
+
+
+def topla():
+  print(a + b)
+
+topla(1, 5)
+
+def topla(a, b):
+  print(a + b)
+  
+topla(1, 5) 
+
+
+def çarpma(a, b = 1):
+   print(a * b)
+   
+çarpma(5, 6)
+
+def çarpma(a, b = 1):
+   print(a * b)
+   
+çarpma(5)   
